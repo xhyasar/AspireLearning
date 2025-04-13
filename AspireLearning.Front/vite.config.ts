47f3292744723300c5f9373d74ce2b4c4b3c43a9
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-04-13 19:26:32 +0300

    [FE] App.tsx and dependencies updated.

--- AspireLearning.Front/vite.config.ts
+++ AspireLearning.Front/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Load environment variables
+dotenv.config();
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(),react()],
+  server: {
+    port: Number(process.env.PORT) || 4001,
+    proxy: {
+      '/api': {
+        target: process.env.services__apigateway__https__0 || process.env.services__apigateway__http__0,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'bundle.js',
+      },
+    },
+  },
+});
