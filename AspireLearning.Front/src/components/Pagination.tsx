85aa3a7d0891089b5d0c96377571d43196857969
xaekaraca <ahmetemre_k@hotmail.com>
2025-06-27 21:51:51 +0300

    [FE] Remove redundant React imports from components

--- AspireLearning.Front/src/components/Pagination.tsx
+++ AspireLearning.Front/src/components/Pagination.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 // İkonlar
 const FirstPageIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
--- AspireLearning.Front/src/components/TableControls.tsx
+++ AspireLearning.Front/src/components/TableControls.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface TableControlsProps {
     onExport: () => void;
     onAdd: () => void;
